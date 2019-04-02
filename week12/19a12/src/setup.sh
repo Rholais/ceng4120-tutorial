d6f53e094fef9c3480faa34461595f2932bf2eea
LI Haocheng <hcli@cse.cuhk.edu.hk>
2019-04-02 15:48:23 +0800

    create multiplyadd.tcl

---
+++ week12/19a12/src/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/tcsh
+
+setenv CDS_ROOT /opt2/cadence
+setenv CDS_LIC_FILE /opt2/cadence/cds.lic
+setenv LM_LICENSE_FILE /opt2/cadence/cds.lic
+
+setenv PATH /opt2/cadence/INNOVUS162/bin:$PATH
+
