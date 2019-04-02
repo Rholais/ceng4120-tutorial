e61ed9b738bc3ba133a6a9c1f01afc446618315e
LI Haocheng <hcli@cse.cuhk.edu.hk>
2019-04-02 21:42:16 +0800

    update innovus

--- week12/19a12/src/setup.sh
+++ week12/19a12/src/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/tcsh
 
-setenv CDS_ROOT /opt2/cadence
-setenv CDS_LIC_FILE /opt2/cadence/cds.lic
-setenv LM_LICENSE_FILE /opt2/cadence/cds.lic
+setenv CDS_ROOT /research/fyyoung/hcli/opt/cadence
+setenv CDS_LIC_FILE /research/fyyoung/hcli/opt/cadence/cds.lic
+setenv LM_LICENSE_FILE /research/fyyoung/hcli/opt/cadence/cds.lic
 
-setenv PATH /opt2/cadence/INNOVUS162/bin:$PATH
+setenv PATH /research/fyyoung/hcli/opt/cadence/INNOVUS171/bin:$PATH
 
