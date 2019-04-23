06d6583b24c16c8a2c4b1702ecc750ef8aef0a3b
Rholais Lii <rholais@gmail.com>
2019-04-23 18:49:58 +0800

    Create test5.in

--- week13/gen/gen.py
+++ week13/gen/gen.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
+import math
+import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-dies = [0,      200000, 400000, 500000, 600000, 800000, 900000, 1000000]
-n_tapss = [0,   4,      8,      12,     20,     24,     28,     32]
-n_lvss = [0,    1,      2,      2,      2,      3,      3,      3]
-n_ch1ss = [0,   10,     6,      8,      8,      5,      6,      7]
-n_ch2ss = [0,   10,     3,      3,      4,      3,      4,      5]
-n_ch3ss = [0,   10,     3,      3,      4,      3,      4,      5]
-n_pinss = [0,   4000,   5000,   6000,   7000,   8000,   9000,   10000]
-dists = [0,     300000, 300000, 410000, 420000, 440000, 470000, 490000]
-clones = [0,    0.2,    0.2,    0.2,    0.2,    0.2,    0.2,    0.2]
-loads = [0,     2000,   1200,   1000,   800,    800,    800,    800]
+dies = [0,      200000,     400000,     500000,     600000,     800000,     900000,
+        0,      1000000,    1100000,    1200000,    1300000,    1400000,    1500000]
+n_tapss = [0,   4,          8,          12,         20,         20,         28,
+           0,   22,         24,         26,         28,         30,         32]
+n_grpss = [0,   10,         24,         32,         40,         41,         126,
+           0,   70,         99,         128,        157,        186,        215]
+n_pinss = [0,   4000,       5000,       6000,       7000,       7000,       9000,
+           0,   7500,       8000,       8500,       9000,       9500,       10000]
+dists = [0,     300000,     300000,     410000,     420000,     460000,     470000,
+         0,     480000,     490000,     500000,     510000,     520000,     530000]
+clones = [0,    0.2,        0.2,        0.2,        0.2,        0.2,        0.2,
+          0,    0.2,        0.2,        0.2,        0.2,        0.2,        0.2]
+loads = [0,     2000,       1200,       1000,       800,        2100,       800,
+         0,     2100,       2100,       2100,       2100,       2100,       2100]
 
 
 def check_dup(a, name):
@@ -22,7 +28,7 @@ def check_dup(a, name):
         print('duplicated {0}s: {1}'.format(name, dup))
 
 
-def check_vio(taps, pins, tap_group, dist, n_groups, clone, load, name):
+def check_vio(taps, pins, tap_grp, dist, n_grps, clone, load, name):
     # print('check {0}'.format(name))
 
     is_slower = False
@@ -37,7 +43,7 @@ def check_vio(taps, pins, tap_group, dist, n_groups, clone, load, name):
     #     print('max distance is {0}'.format(pins[:, 3].max()))
 
     is_larger = False
-    c = tap_group.sum() / n_groups - 1
+    c = tap_grp.sum() / n_grps - 1
     if c > clone:
         # print('clone {0} is more than {1}'.format(c, clone))
         is_larger = True
@@ -54,7 +60,7 @@ def check_vio(taps, pins, tap_group, dist, n_groups, clone, load, name):
     return is_slower, is_larger, is_heavier
 
 
-for i in range(6, 8):
+for i in [5, 8, 9, 10, 11, 12, 13]:
     die = dies[i]
     n_taps = n_tapss[i]
     taps = np.zeros((n_taps, 3), dtype=np.int64)
@@ -65,38 +71,29 @@ for i in range(6, 8):
     check_dup(taps, 'tap')
 
     n_pins = n_pinss[i]
-    n_ch1s = n_ch1ss[i]
-    n_ch2s = n_ch2ss[i]
-    n_ch3s = n_ch3ss[i]
-    n_groups = n_ch1s
-    n_lvs = n_lvss[i]
-    if n_lvs == 3:
-        n_groups += n_ch1s * n_ch2s + n_ch1s * n_ch2s * n_ch3s
-    elif n_lvs == 2:
-        n_groups += n_ch1s * n_ch2s
-
-    groups = np.zeros((n_groups, 6), dtype=np.int64)
-    for j in range(0, n_groups):
+    n_grps = n_grpss[i]
+    grps = np.zeros((n_grps, 6), dtype=np.int64)
+    for j in range(0, n_grps):
         for k in range(0, 2):
-            groups[j, k] = random.randrange(die / 4)
+            grps[j, k] = random.randrange(die / 4)
 
     pins = np.zeros((n_pins, 5), dtype=np.int64)
     for j in range(0, n_pins):
-        g = random.randrange(n_groups)
+        g = random.randrange(n_grps)
         pins[j, 2] = g
         for k in range(0, 2):
-            pins[j, k] = groups[g, k] + random.randrange(die * 3 / 4)
+            pins[j, k] = grps[g, k] + random.randrange(die * 3 / 4)
 
     check_dup(pins[:, 0:2], 'pin')
 
     dist = dists[i]
     clone = clones[i]
     load = loads[i]
-    tap_group = np.zeros((n_taps, n_groups), dtype=int)
+    tap_grp = np.zeros((n_taps, n_grps), dtype=int)
     while True:
         print('dist {0}\t clone {1}\tload {2}'.format(dist, clone, load))
         taps[:, 2] = 0
-        tap_group = np.zeros((n_taps, n_groups), dtype=int)
+        tap_grp = np.zeros((n_taps, n_grps), dtype=int)
         for j in range(0, n_pins):
             d = np.absolute(taps[:, 0:2] - pins[j, 0:2]).sum(axis=1)
             pins[j, 3] = d.min()
@@ -104,57 +101,43 @@ for i in range(6, 8):
             pins[j, 4] = t
             taps[t, 2] += 1
             g = pins[j, 2]
-            tap_group[t, g] = 1
-            if n_lvs == 3 and g >= n_ch1s * (1 + n_ch2s):
-                ppg = ((g - n_ch1s) // n_ch2s - n_ch1s) // n_ch3s
-                tap_group[t, ppg] = 1
-                tap_group[t, (g - n_ch1s * (1 + n_ch2s)) //
-                          n_ch3s - n_ch2s * ppg] = 1
-            elif n_lvs >= 2 and g >= n_ch1s:
-                tap_group[t, (g - n_ch1s) // n_ch2s] = 1
+            tap_grp[t, g] = 1
 
         is_slower, is_larger, is_heavier = check_vio(
-            taps, pins, tap_group, dist, n_groups, clone, load, 'dist')
+            taps, pins, tap_grp, dist, n_grps, clone, load, 'dist')
         if not (is_slower or is_larger or is_heavier):
             break
 
-        groups[:, 2:5] = np.zeros((n_groups, 3), dtype=np.int64)
+        grps[:, 2:5] = np.zeros((n_grps, 3), dtype=np.int64)
         for j in range(0, n_pins):
             g = pins[j, 2]
-            groups[g, 2:4] += pins[j, 0:2]
-            groups[g, 4] += 1
+            grps[g, 2:4] += pins[j, 0:2]
+            grps[g, 4] += 1
 
-        if not groups[:, 4].min():
-            print('group {0} is empty'.format(groups[:, 2].argmin()))
+        if not grps[:, 4].min():
+            print('group {0} is empty'.format(grps[:, 2].argmin()))
 
-        centers = np.zeros((n_groups, 2))
+        centers = np.zeros((n_grps, 2))
         for j in range(0, 2):
-            centers[:, j] = groups[:, j + 2] / groups[:, 4]
+            centers[:, j] = grps[:, j + 2] / grps[:, 4]
 
         taps[:, 2] = 0
-        for j in range(0, n_groups):
+        for j in range(0, n_grps):
             t = np.absolute(taps[:, 0:2] - centers[j, 0:2]
                             ).sum(axis=1).argmin()
-            groups[j, 5] = t
-            taps[t, 2] += groups[j, 4]
+            grps[j, 5] = t
+            taps[t, 2] += grps[j, 4]
 
-        tap_group = np.zeros((n_taps, n_groups), dtype=int)
+        tap_grp = np.zeros((n_taps, n_grps), dtype=int)
         for j in range(0, n_pins):
             g = pins[j, 2]
-            t = groups[g, 5]
+            t = grps[g, 5]
             pins[j, 3] = np.absolute(taps[t, 0:2] - pins[j, 0:2]).sum()
             pins[j, 4] = t
-            tap_group[t, g] = 1
-            if n_lvs == 3 and g >= n_ch1s * (1 + n_ch2s):
-                ppg = ((g - n_ch1s) // n_ch2s - n_ch1s) // n_ch3s
-                tap_group[t, ppg] = 1
-                tap_group[t, (g - n_ch1s * (1 + n_ch2s)) //
-                          n_ch3s - n_ch2s * ppg] = 1
-            elif n_lvs >= 2 and g >= n_ch1s:
-                tap_group[t, (g - n_ch1s) // n_ch2s] = 1
+            tap_grp[t, g] = 1
 
-        s, l, h = check_vio(taps, pins, tap_group, dist,
-                            n_groups, clone, load, 'group')
+        s, l, h = check_vio(taps, pins, tap_grp, dist,
+                            n_grps, clone, load, 'group')
         if not (s or l or h):
             break
 
@@ -166,65 +149,43 @@ for i in range(6, 8):
             dist *= 1.01
 
         if is_larger:
-            clone *= 1.01
+            clone *= 1.00
 
         if is_heavier:
             load *= 1.01
 
         c = pins[:, 0:2].mean(axis=0, dtype=float)
+        cgm = math.sqrt(np.power(centers - c, 2).sum(axis=1).max())
         for j in range(0, n_pins):
+            g = pins[j, 2]
+            cg = centers[g]
+            cga = abs(cg - c) / cgm
+            pins[j, 0:2] = (pins[j, 0:2] - c) * 0.984 + (cg - c) * 0.008 + \
+                (random.randrange(die) - c) * 0.001
             for k in range(0, 2):
-                g = pins[j, 2]
-                pg = pins[j, 2]
-                ppg = pins[j, 2]
-                if n_lvs == 3 and g >= n_ch1s * (1 + n_ch2s):
-                    ppg = ((g - n_ch1s) // n_ch2s - n_ch1s) // n_ch3s
-                    pg = (g - n_ch1s * (1 + n_ch2s)) // n_ch3s - n_ch2s * ppg
-                elif n_lvs >= 2 and g >= n_ch1s:
-                    pg = (g - n_ch1s) // n_ch2s
+                if cg[k] > c[k]:
+                    pins[j, k] += (die * cga[k] * 0.004)
+                elif cg[k] < c[k]:
+                    pins[j, k] -= (die * cga[k] * 0.004)
 
-                cg = centers[g, k]
-                cpg = centers[pg, k]
-                cppg = centers[ppg, k]
-                pins[j, k] = pins[j, k] * 0.90 + cg * 0.01 + cpg * \
-                    0.04 + cppg * 0.00 + random.randrange(die) * 0.001
-                if cg > c[k]:
-                    pins[j, k] += (die - 1) * 0.05
-                elif cg == c[k]:
-                    pins[j, k] += c[k] * 0.05
+                pins[j, k] += c[k]
 
-    print(np.sum(tap_group.sum(axis=1) > 0))
+    print(np.sum(tap_grp.sum(axis=1) > 0))
+    plt.scatter(pins[:, 0], pins[:, 1], c=pins[:, 2], alpha=0.5)
+    plt.show()
+
     f = open('test{0}.in'.format(i), 'w')
     f.write('MAX_RUNTIME 3600\n')
-    f.write('MAX_DISTANCE {0}\n'.format(dist / 1.01))
-    f.write('MAX_CLONE {0}\n'.format(clone / 1.01))
-    f.write('MAX_LOAD {0}\n\n'.format(load / 1.01))
+    f.write('MAX_DISTANCE {0}\n'.format(dist / 1.02))
+    f.write('MAX_CLONE {0}\n'.format(clone / 1.00))
+    f.write('MAX_LOAD {0}\n'.format(load / 1.02))
+    f.write('NUM_GROUPS {0}\n\n'.format(n_grps))
 
     f.write('TAPS {0}\n'.format(n_taps))
     for j in range(0, n_taps):
         f.write('    TAP {0} {1} {2}\n'.format(j, taps[j, 0], taps[j, 1]))
 
     f.write('END TAPS\n\n')
-
-    f.write('GROUPS {0}\n'.format(n_groups))
-    if n_lvs >= 1:
-        for j in range(0, n_ch1s):
-            f.write('    GROUP {0}\n'.format(j))
-            if n_lvs >= 2:
-                for k in range(0, n_ch2s):
-                    pg = n_ch1s + j * n_ch2s + k
-                    f.write('        GROUP {0}\n'.format(pg))
-                    if n_lvs >= 3:
-                        for l in range(0, n_ch3s):
-                            g = n_ch1s + n_ch1s * n_ch2s + j * n_ch2s * n_ch3s + k * n_ch3s + l
-                            f.write('            GROUP {0}\n'.format(g))
-                            f.write('            END {0}\n'.format(g))
-
-                    f.write('        END {0}\n'.format(pg))
-
-            f.write('    END {0}\n'.format(j))
-
-    f.write('END GROUPS\n\n')
 
     f.write('PINS {0}\n'.format(n_pins))
     for j in range(0, n_pins):
