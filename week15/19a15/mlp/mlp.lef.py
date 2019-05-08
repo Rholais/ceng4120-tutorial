c8cd2f3762657fc2c2d7b8dde52ea9c2130eb9cb
LI Haocheng <hcli@cse.cuhk.edu.hk>
2019-05-08 22:53:44 +0800

    update mlp.lef.py

--- week15/19a15/mlp/mlp.lef.py
+++ week15/19a15/mlp/mlp.lef.py
@@ -985,14 +985,6 @@ SITE CoreSite
   CLASS CORE ;
   SIZE 0.1 BY 1.2 ;
 END CoreSite
-
-MACRO NEURON
-    CLASS CORE ;
-    FOREIGN NEURON 0.000000 0.000000 ;
-    ORIGIN 0.000000 0.000000 ;
-    SIZE 6.400000 BY 1.200000 ;
-    SYMMETRY X Y ;
-    SITE CoreSite ;
 '''
 
 END = '''
@@ -1014,39 +1006,59 @@ END = '''
         RECT 0.000000 -0.080000 6.400000 0.080000 ;
         END
     END VSS
-END NEURON
-
-END LIBRARY
-
 '''
 
 with open('mlp.lef', 'w') as f:
     f.write(BEGIN)
-    for i in range(0, 5):
-        for j in range(0, 8):
-            f.write('    PIN A{0}{1}\n'.format(i, j))
-            f.write('        DIRECTION INPUT ;\n')
-            f.write('        USE SIGNAL ;\n')
-            f.write('        PORT\n')
-            f.write('        LAYER Metal1 ;\n')
-            x = j * 0.5 + 0.25
-            y = i * 0.2 + 0.2
-            f.write('        RECT {0:.3f} {1:.3f} {2:.3f} {3:.3f} ;\n'.format(x - 0.025, y - 0.055, x + 0.025, y + 0.055))
-            f.write('        END\n')
-            f.write('    END A{0}{1}\n'.format(i, j))
-
     for i in range(0, 2):
-        for j in range(0, 4):
-            f.write('    PIN Y{0}\n'.format(i * 4 + j))
-            f.write('        DIRECTION OUTPUT ;\n')
-            f.write('        USE SIGNAL ;\n')
-            f.write('        PORT\n')
-            f.write('        LAYER Metal1 ;\n')
-            x = j * 0.5 + 4.65
-            y = i * 0.4 + 0.4
-            f.write('        RECT {0:.3f} {1:.3f} {2:.3f} {3:.3f} ;\n'.format(x - 0.025, y - 0.055, x + 0.025, y + 0.055))
-            f.write('        END\n')
-            f.write('    END Y{0}\n'.format(i * 4 + j))
+        if i:
+            name = 'NEUROS'
+        else:
+            name = 'NEURON'
 
-    f.write(END)
+        f.write('MACRO {0}\n'.format(name))
+        f.write('    CLASS CORE ;\n')
+        f.write('    FOREIGN {0} 0.000000 0.000000 ;\n'.format(name))
+        f.write('    ORIGIN 0.000000 0.000000 ;\n')
+        f.write('    SIZE 6.400000 BY 1.200000 ;\n')
+        f.write('    SYMMETRY X Y ;\n')
+        f.write('    SITE CoreSite ;\n')
+        for j in range(0, 5):
+            for k in range(0, 8):
+                f.write('    PIN A{0}{1}\n'.format(j, k))
+                f.write('        DIRECTION INPUT ;\n')
+                f.write('        USE SIGNAL ;\n')
+                f.write('        PORT\n')
+                f.write('        LAYER Metal1 ;\n')
+                x = k * 0.5 + 0.25
+                if i:
+                    y = -j * 0.2 + 1.0
+                else:
+                    y = +j * 0.2 + 0.2
+
+                f.write('        RECT {0:.3f} {1:.3f} {2:.3f} {3:.3f} ;\n'.format(x - 0.025, y - 0.055, x + 0.025, y + 0.055))
+                f.write('        END\n')
+                f.write('    END A{0}{1}\n'.format(j, k))
+
+        for j in range(0, 2):
+            for k in range(0, 4):
+                f.write('    PIN Y{0}\n'.format(j * 4 + k))
+                f.write('        DIRECTION OUTPUT ;\n')
+                f.write('        USE SIGNAL ;\n')
+                f.write('        PORT\n')
+                f.write('        LAYER Metal1 ;\n')
+                x = k * 0.5 + 4.65
+                if i:
+                    y = -j * 0.4 + 0.8
+                else:
+                    y = +j * 0.4 + 0.4
+
+                f.write('        RECT {0:.3f} {1:.3f} {2:.3f} {3:.3f} ;\n'.format(x - 0.025, y - 0.055, x + 0.025, y + 0.055))
+                f.write('        END\n')
+                f.write('    END Y{0}\n'.format(j * 4 + k))
+
+        f.write(END)
+        f.write('END {0}\n\n'.format(name))
+
+    f.write('END LIBRARY\n\n')
 
